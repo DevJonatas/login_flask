b4a4d50d7a48fd3551fa456b73c475046245d345
Jonatas Silva <33060900+DevJonatas@users.noreply.github.com>
2019-03-06 22:14:18 -0300

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def home():
     if not session.get('logged_in'):
         return render_template('login.html')
     else:
-        return "Hello Boss!"
+        return "Olá!"
 
 @app.route('/login', methods=['POST'])
 def do_admin_login():
